06aed91e3aef98f3d5f38c46bf998f661e5be519
Omar0Nosseir <omarerror2007@gmail.com>
2024-01-03 04:26:20 +0200

    Loser

---
+++ Todo.js
@@ -0,0 +1,42 @@
+const inputBox = document.getElementById("input-box");
+const Listcontainer = document.getElementById("list-container");
+
+function addtask()
+{
+    if(inputBox.value == '')
+    {
+        alert("You must write somthing!");
+    }
+    else
+    {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        Listcontainer.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
+        li.appendChild(span);
+    }
+    inputBox.value="";
+    save();
+}
+Listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName == "LI"){
+        e.target.classList.toggle("checked");
+        save();
+    }
+    else if(e.target.tagName == "SPAN"){
+        e.target.parentElement.remove();
+        save();
+    }
+},false);
+
+function save()
+{
+    localStorage.setItem("data",Listcontainer.innerHTML);
+}
+function displaydata()
+{
+    Listcontainer.innerHTML = localStorage.getItem("data");
+}
+displaydata();
